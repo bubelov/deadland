baf7ba2441517871f5098e46ef3a9eccf4f15d84
Igor Bubelov <ibubelov@gmail.com>
2014-08-09 15:20:10 +0400

    Foobar

--- core/src/com/deadland/model/Zombie.java
+++ core/src/com/deadland/model/Zombie.java
@@ -1,8 +1,11 @@
 package com.deadland.model;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
+import com.deadland.EntityManager;
 
 /**
  * Author: Igor Bubelov
@@ -13,6 +16,8 @@ public class Zombie extends Entity {
     public static Texture texture = new Texture("zombie.jpeg");
 
     public Vector2 destination = null;
+
+    public float moveTimeoutSeconds = MathUtils.random(0, 40);
 
     public Zombie(float x, float y) {
         sprite = new Sprite(texture);
@@ -27,13 +32,37 @@ public class Zombie extends Entity {
     public void update() {
         super.update();
 
-        if (destination != null){
+        boolean attacking = false;
+
+        for (Entity entity : EntityManager.instance.entities) {
+            if (entity instanceof Hero) {
+                Hero hero = (Hero)entity;
+
+                if (Vector2.dst(sprite.getX(), sprite.getY(), hero.sprite.getX(), hero.sprite.getY()) < 300) {
+                    attacking = true;
+                    destination = new Vector2(hero.sprite.getX(), hero.sprite.getY());
+                }
+            }
+        }
+
+        if (destination != null) {
             Vector2 movementVector = new Vector2(destination);
             movementVector.sub(sprite.getX(), sprite.getY());
 
             sprite.setRotation(movementVector.angle());
             sprite.translate(movementVector.nor().x * 3, movementVector.nor().y * 3);
 
+        }
+
+        if (!attacking && destination != null && destination.dst(sprite.getX(), sprite.getY()) < 5) {
+            destination = null;
+            moveTimeoutSeconds = MathUtils.random(0, 40);
+        }
+
+        moveTimeoutSeconds -= Gdx.graphics.getDeltaTime();
+
+        if (destination == null && moveTimeoutSeconds <= 0) {
+            destination = new Vector2(sprite.getX() + MathUtils.random(-100, 100), sprite.getY() + MathUtils.random(-100, 100));
         }
     }
 }
